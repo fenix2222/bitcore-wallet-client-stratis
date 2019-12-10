f0db2c1ed9f1805a992efc80e1998c4e64ece178
Rowan de Haas <rowandehaas@outlook.com>
2019-12-09 21:36:40 -0800

    Add tsbuild

---
+++ ts_build/lib/common/constants.d.ts
@@ -0,0 +1,131 @@
+export declare const Constants: {
+    SCRIPT_TYPES: {
+        P2SH: string;
+        P2PKH: string;
+    };
+    DERIVATION_STRATEGIES: {
+        BIP44: string;
+        BIP45: string;
+        BIP48: string;
+    };
+    PATHS: {
+        SINGLE_ADDRESS: string;
+        REQUEST_KEY: string;
+        REQUEST_KEY_AUTH: string;
+    };
+    BIP45_SHARED_INDEX: number;
+    UNITS: {
+        btc: {
+            toSatoshis: number;
+            full: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+            short: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+        };
+        bch: {
+            toSatoshis: number;
+            full: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+            short: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+        };
+        eth: {
+            toSatoshis: number;
+            full: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+            short: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+        };
+        usdc: {
+            toSatoshis: number;
+            full: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+            short: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+        };
+        pax: {
+            toSatoshis: number;
+            full: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+            short: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+        };
+        gusd: {
+            toSatoshis: number;
+            full: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+            short: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+        };
+        bit: {
+            toSatoshis: number;
+            full: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+            short: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+        };
+        strat: {
+            toSatoshis: number;
+            full: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+            short: {
+                maxDecimals: number;
+                minDecimals: number;
+            };
+        };
+    };
+    COINS: string[];
+    ERC20: string[];
+    UTXO_COINS: string[];
+    TOKEN_OPTS: {
+        '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': {
+            name: string;
+            symbol: string;
+            decimal: number;
+            address: string;
+        };
+        '0x8e870d67f660d95d5be530380d0ec0bd388289e1': {
+            name: string;
+            symbol: string;
+            decimal: number;
+            address: string;
+        };
+        '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd': {
+            name: string;
+            symbol: string;
+            decimal: number;
+            address: string;
+        };
+    };
+};
+//# sourceMappingURL=constants.d.ts.map
